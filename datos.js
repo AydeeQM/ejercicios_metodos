1bae80f2e5dcb1564931ce6d072414573fa4f3a1
aydee <aydee.qm@gmail.com>
2017-06-24 13:11:10 -0500

    modificacion de pruebas unitarias en datos

--- datos.js
+++ datos.js
@@ -14,88 +14,132 @@ function multiplica(arr, num)
 //document.write(multiplica());
 
 //Ejercicio 2
-//var numCel = [784123123, 355676784, 3423543554];
-//var cifraTres = 784;
-
 function ejercicio2(numCel, cifraTres){
-var cifras = numCel.filter(function igual (num){
-    var num  = num.toString();
-    var contar = cifraTres;
-    if((num.substr(0,3)==contar) || (num.substr(-3)==contar))
-    {
-      return true;
-    }
-  });
+  var cifras = numCel.filter(function igual (num){
+      var num  = num.toString();
+      var contar = cifraTres;
+      if((num.substr(0,3)==contar) || (num.substr(-3)==contar))
+      {
+        return true;
+      }
+    });
+  return cifras;
 }
  
-//document.write(cifras);
+//document.write(ejercicio2());
 
 //Ejercicio 3
-var arr = [7, 21, 44, 80, 77, 35];
-var multiplo = 7;
-var res="";
-arr.forEach(function resolver (num){
+function ejercicio3(arr, multiplo){
+  var res= [];
+  arr.forEach(function resolver (num){
   if(num%multiplo==0){
-    res +=num + " ";
+    res.push(num);
   }
 });
+  return res;
+}
+
 //document.write(res);
 
 //Ejercicio 4
-var superArreglo = [2, 5, 7, 2, 3, 8, 10]
-var solu = "";
-var dobles = superArreglo.map(function(num){
-  solu += num*2+1 + " ";
+function ejercicio4 (superArreglo){
+  var solu = [];
+  var dobles = superArreglo.map(function(num){
+    solu.push(num*2+1);
 });
+  return solu;
+}
 
 //document.write(solu);
 
 //Ejercicio 5
-var celcius = [0, 10, 20, 30, 40, 50, 60];
-var conversion = "";
-var solucion = celcius.map(function(grados){
-  conversion += (grados*9 + (32*5))/5 + ' ';
-});
+function ejercicio5 (celcius){
+  var conversion = [];
+  var solucion = celcius.map(function(grados){
+    conversion.push((grados*(9/5))+32);
+  });
+  return conversion;
+}
 
-//document.write(conversion);
+//console.log(ejercicio5(celcius));
 
 //Ejercicio 6
-var multi = [1, 2, 3, 4];
-
-var total= multi.reduce(function(producto, siguiente){
+function ejercicio6 (multi){
+  var total= multi.reduce(function(producto, siguiente){
   return producto * siguiente;
   }, 1);
+  return total;
+}
 
-//document.write(total);
+//console.log(ejercicio6(multi));
 
 //Ejercicio 7
-var edades = [20, 16, 89, 6];
-var resulk="";
-edades.filter(function add (number){
+function ejercicio7(edades){
+  var resulk = [];
+  edades.filter(function add (number){
   if(number%2==0 && number>18){
-     resulk += 2017 - number;
-  }
-});
-
-//document.write(resulk);
+     resulk.push(2017 - number);
+    }
+  });
+    return resulk;
+}
+//console.log(ejercicio7(edades));
 
 //Pruebas Unitarias
-/*describe('Ejercicio 1', function(){
-    it('Prueba de [2, 3] y 10', function(){
-        assert.equal(60, multiplica([2,3], 10));
-    });
-    it('Prueba de [2, 3] y "hola"', function(){
-        assert.equal(6, multiplica([2,3], "hola"));
-    });
-    it('Prueba de [2, 3] y "hola"', function(){
-        assert.equal(6, multiplica([2,3]));
-    });
-});*/
-
-describe('Ejercicio 2', function(){
-    it('Cuando es [784123123, 355676784, 3423543554] y 784', function(){
-        assert.deepEqual([784123123], ejercicio2([784123123, 355676794, 3423543554], 784));
+describe('Ejercicios Arrays', function(){
+    describe('Ejercicio 1', function(){
+        it('Prueba de [2, 3] y 10', function(){
+            assert.equal(60, multiplica([2,3], 10));
+        });
+        it('Prueba de [2, 3] y "hola"', function(){
+            assert.equal(6, multiplica([2,3], "hola"));
+        });
+        it('Prueba de [2, 3] y "hola"', function(){
+            assert.equal(6, multiplica([2,3]));
+        });
     });
 
+    describe('Ejercicio 2', function(){
+        it('Cuando es [784123123, 355676784, 3423543554] y 784', function(){
+            assert.deepEqual([784123123], ejercicio2([784123123, 355676794, 3423543554], 784));
+        });
+        it('Cuando es [784123123, 355676784, 3423543554] y 123', function(){
+            assert.deepEqual([784123123], ejercicio2([784123123, 355676794, 3423543554], 784));
+        });
+
+    });
+    describe('Ejercicio 3', function(){
+        it('Cuando es [7, 21, 44, 80, 77, 35] y 7', function(){
+            assert.deepEqual([7, 21, 77, 35], ejercicio3([7, 21, 44, 80, 77, 35],  7));
+        });
+
+    });
+    
+    describe('Ejercicio 4', function(){
+        it('Cuando es [2, 5, 7, 2, 3, 8, 10]', function(){
+            assert.deepEqual([5, 11, 15, 5, 7, 17, 21], ejercicio4([2, 5, 7, 2, 3, 8, 10], 2, 1));
+        });
+
+    });
+    describe('Ejercicio 5', function(){
+        it('Cuando es [0, 10, 20, 30, 40, 50, 60]', function(){
+            assert.deepEqual([32, 50, 68, 86, 104, 122, 140], ejercicio5([0, 10, 20, 30, 40, 50, 60],9 ,5 ,32));
+        });
+
+    });
+    describe('Ejercicio 6', function(){
+        it('Cuando es [1, 2, 3, 4]', function(){
+            assert.deepEqual(24, ejercicio6([1, 2, 3, 4]));
+        });
+
+    });
+    
+    describe('Ejercicio 7', function(){
+        it('Cuando es [20, 16, 89, 6]', function(){
+            assert.deepEqual([1997], ejercicio7([20, 16, 89, 6]));
+        });
+
+    });
+    
 });
 
